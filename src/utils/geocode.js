a57c5ebfb767cfd3f50b5ae8892f148a69ea8a31
Aditya <adityasingh2640@gmail.com>
2019-11-10 22:02:35 +0530

    Refactor for token error

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,12 +1,12 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYWRpdHlhc2luZ2gyNjQwIiwiYSI6ImNrMnBsZjd1cTA1M3kzbHA3dGViM2g0bzgifQ.5a7HkrDg-JY0YeeJHxhjkg&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+        } else if (body.features && body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
