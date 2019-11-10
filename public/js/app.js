9b876b1a7d9f0d6e61f47897c11c9911d7315211
Aditya <adityasingh2640@gmail.com>
2019-11-10 22:14:08 +0530

    Heroku update

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -13,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
